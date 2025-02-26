ccb613bfd0592658951ff144c80c5eeede1450a3
mohdirshad01 <102645245+mohdirshad01@users.noreply.github.com>
2025-02-26 16:07:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    bot_token: '7817058755:AAFOBnJLoTtUzOELSg2mE6AMOD7UDTfZjFo',
+    bot_token: '7486914321:AAG80OX-8xex5Ls6BdGelFu5K-yijiqOXu8',
     admins: [5547959277],
     mongo_url: `mongodb+srv://technoearneryt:QjjI4nsWQ9tYhSXZ@cluster0.m1bxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
 };
